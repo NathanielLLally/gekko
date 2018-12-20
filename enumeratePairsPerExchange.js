d7539dc4d7cc9f596b3e47570f26f2125b31d05f
Nathaniel Lally <nathaniel@mulberrystreet.software>
2018-12-20 00:22:46 +0000

    new prorams

---
+++ enumeratePairsPerExchange.js
@@ -0,0 +1,49 @@
+const util = require(__dirname + '/core/util');
+const dirs = util.dirs();
+
+const _ = require('lodash');
+const events = require('events');
+const moment = require('moment');
+//const orders = require('./orders');
+//const Trigger = require('./trigger');
+//const exchangeUtils = require('./exchangeUtils');
+//const bindAll = exchangeUtils.bindAll;
+const log = require(dirs.core + 'log');
+const Broker = require(dirs.broker + 'gekkoBroker');
+const Trader = require(dirs.plugins + 'trader/trader').Trader;
+const JSON = require('JSON');
+
+//const Trader = require(__dirname  + '/exchange/wrappers/gdax');
+
+require('./exchange/dependencyCheck');
+
+//globals
+const config = util.getConfig();
+
+
+if (_.has(config, 'list_update_import')) {
+  console.log("has configured exchanges: ");
+}
+
+//exchange/util/genMarketFiles
+
+//main
+var trader = new Trader(() => {});
+
+trader.init();
+
+trader.on('postInit', () => {
+		console.log('\tGekko v' + util.getVersion() + " using exchange " + config.watch.exchange);
+//	console.log("market config: "+JSON.stringify(trader.broker.marketConfig));
+//	console.log("capabilities: "+JSON.stringify(trader.broker.capabilities));
+	
+//	console.log(trader.broker.capabilities);
+  beginEventLoop = true;
+});
+
+(function wait () {
+  if (beginEventLoop)
+    trader.broadcastDeferredEmit();
+  setTimeout(wait, 50);
+})();
+
